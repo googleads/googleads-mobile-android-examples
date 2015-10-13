a45fe2b4e31bb76b24c3f30784a915475e12c4c5
Andrew Brogdon <redbrogdon@google.com>
2015-10-13 16:06:00 -0700

    Updates examples to latest versions of Play services and tools.

--- admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
+++ admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
@@ -39,7 +39,7 @@ public class MyActivity extends ActionBarActivity {
         // values/strings.xml.
         mAdView = (AdView) findViewById(R.id.ad_view);
 
-        // Create an ad request. Check logcat output for the hashed device ID to
+        // Create an ad request. Check your logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
