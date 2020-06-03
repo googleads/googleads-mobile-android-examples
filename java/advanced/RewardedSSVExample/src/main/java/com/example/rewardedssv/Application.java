36f691e8c289be30d957ed3805851d048a2a0983
Mobile Ads Developer Relations <mad-devrel@google.com>
2020-06-03 03:58:04 -0700

    add RewardedSSV spring-boot Example
    PiperOrigin-RevId: 314503616

---
+++ java/advanced/RewardedSSVExample/src/main/java/com/example/rewardedssv/Application.java
@@ -0,0 +1,21 @@
+package com.example.rewardedssv;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+/** Application entry point */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {};
+  }
+}
