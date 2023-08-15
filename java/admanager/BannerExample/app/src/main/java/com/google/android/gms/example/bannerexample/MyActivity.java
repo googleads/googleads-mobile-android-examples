6c3236343b682b3e2ff3235fa08ca5b3ba742817
Justin Malandruccolo <jmalandruccolo@google.com>
2023-08-15 06:56:01 -0700

    Added UMP SDK integration to Ad Manager Banner Java & Kotlin samples
    PiperOrigin-RevId: 557119541

--- java/admanager/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
+++ java/admanager/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
@@ -18,6 +18,11 @@ package com.google.android.gms.example.bannerexample;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.admanager.AdManagerAdRequest;
@@ -25,73 +30,139 @@ import com.google.android.gms.ads.admanager.AdManagerAdView;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Main Activity. Inflates main activity xml and child fragments.
  */
 public class MyActivity extends AppCompatActivity {
 
-  private AdManagerAdView adView;
   private static final String TAG = "MyActivity";
+  private final AtomicBoolean isMobileAdsInitializeCalled = new AtomicBoolean(false);
+  private AdManagerAdView adView;
+  private GoogleMobileAdsConsentManager googleMobileAdsConsentManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_my);
 
-        // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
-        // values/strings.xml.
-        adView = findViewById(R.id.ad_view);
+    // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+    // values/strings.xml.
+    adView = findViewById(R.id.ad_view);
 
-        // Set your test devices. Check your logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-        // to get test ads on this device."
-        MobileAds.setRequestConfiguration(
-            new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-                                              .build());
+    // Set your test devices. Check your logcat output for the hashed device ID to
+    // get test ads on a physical device. e.g.
+    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+    // to get test ads on this device."
+    MobileAds.setRequestConfiguration(
+        new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345")).build());
 
-        // Log the Mobile Ads SDK version.
-        Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion());
+    // Log the Mobile Ads SDK version.
+    Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion());
 
-        // Initialize the Mobile Ads SDK.
-        MobileAds.initialize(
-            this,
-            new OnInitializationCompleteListener() {
-              @Override
-              public void onInitializationComplete(InitializationStatus status) {}
-            });
+    googleMobileAdsConsentManager = new GoogleMobileAdsConsentManager(this);
+
+    googleMobileAdsConsentManager.gatherConsent(
+        consentError -> {
+          if (consentError != null) {
+            // Consent not obtained in current session.
+            Log.w(
+                TAG,
+                String.format("%s: %s", consentError.getErrorCode(), consentError.getMessage()));
+          }
+
+          if (googleMobileAdsConsentManager.canRequestAds()) {
+            initializeMobileAdsSdk();
+          }
+
+          if (googleMobileAdsConsentManager.isPrivacyOptionsRequired()) {
+            // Regenerate the options menu to include a privacy setting.
+            invalidateOptionsMenu();
+          }
+        });
+
+    // This sample attempts to load ads using consent obtained in the previous session.
+    if (googleMobileAdsConsentManager.canRequestAds()) {
+      initializeMobileAdsSdk();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.action_menu, menu);
+    MenuItem moreMenu = menu.findItem(R.id.action_more);
+    moreMenu.setVisible(googleMobileAdsConsentManager.isPrivacyOptionsRequired());
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    View menuItemView = findViewById(item.getItemId());
+    PopupMenu popup = new PopupMenu(this, menuItemView);
+    popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+    popup.show();
+    popup.setOnMenuItemClickListener(
+        popupMenuItem -> {
+          if (popupMenuItem.getItemId() == R.id.privacy_settings) {
+            // Handle changes to user consent.
+            googleMobileAdsConsentManager.showPrivacyOptionsForm(
+                this,
+                formError -> {
+                  if (formError != null) {
+                    Toast.makeText(this, formError.getMessage(), Toast.LENGTH_SHORT).show();
+                  }
+                });
+            return true;
+          }
+          return false;
+        });
+    return super.onOptionsItemSelected(item);
+  }
+
+  /** Called when leaving the activity */
+  @Override
+  public void onPause() {
+    if (adView != null) {
+      adView.pause();
+    }
+    super.onPause();
+  }
+
+  /** Called when returning to the activity */
+  @Override
+  public void onResume() {
+    super.onResume();
+    if (adView != null) {
+      adView.resume();
+    }
+  }
+
+  /** Called before the activity is destroyed */
+  @Override
+  public void onDestroy() {
+    if (adView != null) {
+      adView.destroy();
+    }
+    super.onDestroy();
+  }
+
+  private void initializeMobileAdsSdk() {
+    if (isMobileAdsInitializeCalled.getAndSet(true)) {
+      return;
+    }
+
+    // Initialize the Mobile Ads SDK.
+    MobileAds.initialize(
+        this,
+        new OnInitializationCompleteListener() {
+          @Override
+          public void onInitializationComplete(InitializationStatus status) {}
+        });
+
     // Create an ad request.
     AdManagerAdRequest adRequest = new AdManagerAdRequest.Builder().build();
-
-        // Start loading the ad in the background.
-        adView.loadAd(adRequest);
-    }
-
-    /** Called when leaving the activity */
-    @Override
-    public void onPause() {
-        if (adView != null) {
-            adView.pause();
-        }
-        super.onPause();
-    }
-
-    /** Called when returning to the activity */
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (adView != null) {
-            adView.resume();
-        }
-    }
-
-    /** Called before the activity is destroyed */
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
+    // Start loading the ad in the background.
+    adView.loadAd(adRequest);
+  }
 }
--- kotlin/admanager/BannerExample/app/src/main/kotlin/com/google/android/gms/example/bannerexample/MyActivity.kt
+++ kotlin/admanager/BannerExample/app/src/main/kotlin/com/google/android/gms/example/bannerexample/MyActivity.kt
@@ -17,11 +17,17 @@ package com.google.android.gms.example.bannerexample
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.android.gms.example.bannerexample.databinding.ActivityMyBinding
+import java.util.concurrent.atomic.AtomicBoolean
 
 private const val TAG = "MainActivity"
 
@@ -29,6 +35,8 @@ private const val TAG = "MainActivity"
 class MyActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMyBinding
+  private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+  private var isMobileAdsInitializeCalled = AtomicBoolean(false)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -46,14 +54,28 @@ class MyActivity : AppCompatActivity() {
       RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
     )
 
-    // Initialize the Mobile Ads SDK with an empty completion listener.
-    MobileAds.initialize(this) {}
+    googleMobileAdsConsentManager = GoogleMobileAdsConsentManager(this)
 
-    // Create an ad request.
-    val adRequest = AdManagerAdRequest.Builder().build()
+    googleMobileAdsConsentManager.gatherConsent { error ->
+      if (error != null) {
+        // Consent not obtained in current session.
+        Log.d(TAG, "${error.errorCode}: ${error.message}")
+      }
 
-    // Start loading the ad in the background.
-    binding.adView.loadAd(adRequest)
+      if (googleMobileAdsConsentManager.canRequestAds) {
+        initializeMobileAdsSdk()
+      }
+
+      if (googleMobileAdsConsentManager.isPrivacyOptionsRequired) {
+        // Regenerate the options menu to include a privacy setting.
+        invalidateOptionsMenu()
+      }
+    }
+
+    // This sample attempts to load ads using consent obtained in the previous session.
+    if (googleMobileAdsConsentManager.canRequestAds) {
+      initializeMobileAdsSdk()
+    }
   }
 
   /** Called when leaving the activity. */
@@ -72,5 +94,48 @@ class MyActivity : AppCompatActivity() {
   public override fun onDestroy() {
     binding.adView.destroy()
     super.onDestroy()
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.action_menu, menu)
+    val moreMenu = menu?.findItem(R.id.action_more)
+    moreMenu?.isVisible = googleMobileAdsConsentManager.isPrivacyOptionsRequired
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val menuItemView = findViewById<View>(item.itemId)
+    val popup = PopupMenu(this, menuItemView)
+    popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
+    popup.show()
+    popup.setOnMenuItemClickListener { popupMenuItem ->
+      when (popupMenuItem.itemId) {
+        R.id.privacy_settings -> {
+          // Handle changes to user consent.
+          googleMobileAdsConsentManager.showPrivacyOptionsForm(this) { formError ->
+            if (formError != null) {
+              Toast.makeText(this@MyActivity, formError.message, Toast.LENGTH_SHORT).show()
+            }
+          }
+          true
+        }
+        else -> false
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun initializeMobileAdsSdk() {
+    if (isMobileAdsInitializeCalled.getAndSet(true)) {
+      return
+    }
+
+    // Initialize the Mobile Ads SDK with an empty completion listener.
+    MobileAds.initialize(this) {}
+
+    // Create an ad request.
+    val adRequest = AdManagerAdRequest.Builder().build()
+    // Start loading the ad in the background.
+    binding.adView.loadAd(adRequest)
   }
 }
