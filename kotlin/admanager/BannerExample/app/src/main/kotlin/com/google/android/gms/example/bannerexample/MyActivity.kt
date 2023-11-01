a99cb4a77eab2848ca446eb81cb673b4bbcc48fc
Jill Song <jillsong@google.com>
2023-11-01 07:33:19 -0700

    Banner example will display an anchored adaptive banner.
    PiperOrigin-RevId: 578511968

--- kotlin/admanager/BannerExample/app/src/main/kotlin/com/google/android/gms/example/bannerexample/MyActivity.kt
+++ kotlin/admanager/BannerExample/app/src/main/kotlin/com/google/android/gms/example/bannerexample/MyActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Google, Inc.
+ * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,19 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.android.gms.example.bannerexample
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.PopupMenu
+import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerAdView
 import com.google.android.gms.example.bannerexample.databinding.ActivityMyBinding
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -35,24 +39,40 @@ private const val TAG = "MainActivity"
 class MyActivity : AppCompatActivity() {
 
   private val isMobileAdsInitializeCalled = AtomicBoolean(false)
+  private val initialLayoutComplete = AtomicBoolean(false)
   private lateinit var binding: ActivityMyBinding
+  private lateinit var adView: AdManagerAdView
   private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+
+  // Determine the screen width (less decorations) to use for the ad width.
+  // If the ad hasn't been laid out, default to the full screen width.
+  private val adSize: AdSize
+    get() {
+      val display = windowManager.defaultDisplay
+      val outMetrics = DisplayMetrics()
+      display.getMetrics(outMetrics)
+
+      val density = outMetrics.density
+
+      var adWidthPixels = binding.adViewContainer.width.toFloat()
+      if (adWidthPixels == 0f) {
+        adWidthPixels = outMetrics.widthPixels.toFloat()
+      }
+
+      val adWidth = (adWidthPixels / density).toInt()
+      return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMyBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    adView = AdManagerAdView(this)
+    binding.adViewContainer.addView(adView)
+
     // Log the Mobile Ads SDK version.
     Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion())
-
-    // Set your test devices. Check your logcat output for the hashed device ID to
-    // get test ads on a physical device. e.g.
-    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-    // to get test ads on this device."
-    MobileAds.setRequestConfiguration(
-      RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
-    )
 
     googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
     googleMobileAdsConsentManager.gatherConsent(this) { error ->
@@ -75,23 +95,39 @@ class MyActivity : AppCompatActivity() {
     if (googleMobileAdsConsentManager.canRequestAds) {
       initializeMobileAdsSdk()
     }
+
+    // Since we're loading the banner based on the adContainerView size, we need to wait until this
+    // view is laid out before we can get the width.
+    binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+      if (!initialLayoutComplete.getAndSet(true) && googleMobileAdsConsentManager.canRequestAds) {
+        loadBanner()
+      }
+    }
+
+    // Set your test devices. Check your logcat output for the hashed device ID to
+    // get test ads on a physical device. e.g.
+    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+    // to get test ads on this device."
+    MobileAds.setRequestConfiguration(
+      RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
+    )
   }
 
   /** Called when leaving the activity. */
   public override fun onPause() {
-    binding.adView.pause()
+    adView.pause()
     super.onPause()
   }
 
   /** Called when returning to the activity. */
   public override fun onResume() {
     super.onResume()
-    binding.adView.resume()
+    adView.resume()
   }
 
   /** Called before the activity is destroyed. */
   public override fun onDestroy() {
-    binding.adView.destroy()
+    adView.destroy()
     super.onDestroy()
   }
 
@@ -104,24 +140,37 @@ class MyActivity : AppCompatActivity() {
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     val menuItemView = findViewById<View>(item.itemId)
-    val popup = PopupMenu(this, menuItemView)
-    popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
-    popup.show()
-    popup.setOnMenuItemClickListener { popupMenuItem ->
-      when (popupMenuItem.itemId) {
-        R.id.privacy_settings -> {
-          // Handle changes to user consent.
-          googleMobileAdsConsentManager.showPrivacyOptionsForm(this) { formError ->
-            if (formError != null) {
-              Toast.makeText(this@MyActivity, formError.message, Toast.LENGTH_SHORT).show()
+    PopupMenu(this, menuItemView).apply {
+      menuInflater.inflate(R.menu.popup_menu, menu)
+      show()
+      setOnMenuItemClickListener { popupMenuItem ->
+        when (popupMenuItem.itemId) {
+          R.id.privacy_settings -> {
+            // Handle changes to user consent.
+            googleMobileAdsConsentManager.showPrivacyOptionsForm(this@MyActivity) { formError ->
+              if (formError != null) {
+                Toast.makeText(this@MyActivity, formError.message, Toast.LENGTH_SHORT).show()
+              }
             }
+            true
           }
-          true
+          else -> false
         }
-        else -> false
       }
+      return super.onOptionsItemSelected(item)
     }
-    return super.onOptionsItemSelected(item)
+  }
+
+  private fun loadBanner() {
+    // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+    adView.adUnitId = "/6499/example/banner"
+    adView.setAdSize(adSize)
+
+    // Create an ad request.
+    val adRequest = AdManagerAdRequest.Builder().build()
+
+    // Start loading the ad in the background.
+    adView.loadAd(adRequest)
   }
 
   private fun initializeMobileAdsSdk() {
@@ -129,12 +178,12 @@ class MyActivity : AppCompatActivity() {
       return
     }
 
-    // Initialize the Mobile Ads SDK with an empty completion listener.
+    // Initialize the Mobile Ads SDK.
     MobileAds.initialize(this) {}
 
-    // Create an ad request.
-    val adRequest = AdManagerAdRequest.Builder().build()
-    // Start loading the ad in the background.
-    binding.adView.loadAd(adRequest)
+    // Load an ad.
+    if (initialLayoutComplete.get()) {
+      loadBanner()
+    }
   }
 }
--- kotlin/admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MainActivity.kt
+++ kotlin/admob/BannerExample/app/src/main/java/com/google/android/gms/example/bannerexample/MainActivity.kt
@@ -1,14 +1,33 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.android.gms.example.bannerexample
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.PopupMenu
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.example.bannerexample.databinding.ActivityMainBinding
@@ -16,33 +35,50 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 private const val TAG = "MainActivity"
 
+/** Main Activity. Inflates main activity xml and child fragments. */
 class MainActivity : AppCompatActivity() {
 
   private val isMobileAdsInitializeCalled = AtomicBoolean(false)
+  private val initialLayoutComplete = AtomicBoolean(false)
   private lateinit var binding: ActivityMainBinding
+  private lateinit var adView: AdView
   private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+
+  // Determine the screen width (less decorations) to use for the ad width.
+  // If the ad hasn't been laid out, default to the full screen width.
+  private val adSize: AdSize
+    get() {
+      val display = windowManager.defaultDisplay
+      val outMetrics = DisplayMetrics()
+      display.getMetrics(outMetrics)
+
+      val density = outMetrics.density
+
+      var adWidthPixels = binding.adViewContainer.width.toFloat()
+      if (adWidthPixels == 0f) {
+        adWidthPixels = outMetrics.widthPixels.toFloat()
+      }
+
+      val adWidth = (adWidthPixels / density).toInt()
+      return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+    }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    adView = AdView(this)
+    binding.adViewContainer.addView(adView)
+
     // Log the Mobile Ads SDK version.
     Log.d(TAG, "Google Mobile Ads SDK Version: " + MobileAds.getVersion())
 
-    // Set your test devices. Check your logcat output for the hashed device ID to
-    // get test ads on a physical device. e.g.
-    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
-    // to get test ads on this device."
-    MobileAds.setRequestConfiguration(
-      RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
-    )
-
     googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
     googleMobileAdsConsentManager.gatherConsent(this) { error ->
-      error?.let {
+      if (error != null) {
         // Consent not obtained in current session.
-        Log.d(TAG, "${it.errorCode}: ${it.message}")
+        Log.d(TAG, "${error.errorCode}: ${error.message}")
       }
 
       if (googleMobileAdsConsentManager.canRequestAds) {
@@ -59,23 +95,39 @@ class MainActivity : AppCompatActivity() {
     if (googleMobileAdsConsentManager.canRequestAds) {
       initializeMobileAdsSdk()
     }
+
+    // Since we're loading the banner based on the adContainerView size, we need to wait until this
+    // view is laid out before we can get the width.
+    binding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+      if (!initialLayoutComplete.getAndSet(true) && googleMobileAdsConsentManager.canRequestAds) {
+        loadBanner()
+      }
+    }
+
+    // Set your test devices. Check your logcat output for the hashed device ID to
+    // get test ads on a physical device. e.g.
+    // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+    // to get test ads on this device."
+    MobileAds.setRequestConfiguration(
+      RequestConfiguration.Builder().setTestDeviceIds(listOf("ABCDEF012345")).build()
+    )
   }
 
-  // Called when leaving the activity
+  /** Called when leaving the activity. */
   public override fun onPause() {
-    binding.adView.pause()
+    adView.pause()
     super.onPause()
   }
 
-  // Called when returning to the activity
+  /** Called when returning to the activity. */
   public override fun onResume() {
     super.onResume()
-    binding.adView.resume()
+    adView.resume()
   }
 
-  // Called before the activity is destroyed
+  /** Called before the activity is destroyed. */
   public override fun onDestroy() {
-    binding.adView.destroy()
+    adView.destroy()
     super.onDestroy()
   }
 
@@ -88,24 +140,37 @@ class MainActivity : AppCompatActivity() {
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     val menuItemView = findViewById<View>(item.itemId)
-    val popup = PopupMenu(this, menuItemView)
-    popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
-    popup.show()
-    popup.setOnMenuItemClickListener { popupMenuItem ->
-      when (popupMenuItem.itemId) {
-        R.id.privacy_settings -> {
-          // Handle changes to user consent.
-          googleMobileAdsConsentManager.showPrivacyOptionsForm(this) { formError ->
-            formError?.let {
-              Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_SHORT).show()
+    PopupMenu(this, menuItemView).apply {
+      menuInflater.inflate(R.menu.popup_menu, menu)
+      show()
+      setOnMenuItemClickListener { popupMenuItem ->
+        when (popupMenuItem.itemId) {
+          R.id.privacy_settings -> {
+            // Handle changes to user consent.
+            googleMobileAdsConsentManager.showPrivacyOptionsForm(this@MainActivity) { formError ->
+              if (formError != null) {
+                Toast.makeText(this@MainActivity, formError.message, Toast.LENGTH_SHORT).show()
+              }
             }
+            true
           }
-          true
+          else -> false
         }
-        else -> false
       }
+      return super.onOptionsItemSelected(item)
     }
-    return super.onOptionsItemSelected(item)
+  }
+
+  private fun loadBanner() {
+    // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+    adView.adUnitId = "ca-app-pub-3940256099942544/9214589741"
+    adView.setAdSize(adSize)
+
+    // Create an ad request.
+    val adRequest = AdRequest.Builder().build()
+
+    // Start loading the ad in the background.
+    adView.loadAd(adRequest)
   }
 
   private fun initializeMobileAdsSdk() {
@@ -113,11 +178,12 @@ class MainActivity : AppCompatActivity() {
       return
     }
 
-    // Initialize the Google Mobile Ads SDK.
-    MobileAds.initialize(this)
+    // Initialize the Mobile Ads SDK.
+    MobileAds.initialize(this) {}
 
-    // Request an ad.
-    val adRequest = AdRequest.Builder().build()
-    binding.adView.loadAd(adRequest)
+    // Load an ad.
+    if (initialLayoutComplete.get()) {
+      loadBanner()
+    }
   }
 }
