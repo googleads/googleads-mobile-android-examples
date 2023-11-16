83d42353611d0d409d355ee68918b09b5501d768
Nicholas Ventimiglia <nventimiglia@google.com>
2023-11-16 14:22:42 -0800

    Corrected Broken AppOpen AdUnitId.
    PiperOrigin-RevId: 583164175

--- java/admob/AppOpenExample/app/src/main/java/com/google/android/gms/example/appopenexample/MyApplication.java
+++ java/admob/AppOpenExample/app/src/main/java/com/google/android/gms/example/appopenexample/MyApplication.java
@@ -128,7 +128,7 @@ public class MyApplication extends Application
   private class AppOpenAdManager {
 
     private static final String LOG_TAG = "AppOpenAdManager";
-    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/3419835294";
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/9257395921";
 
     private final GoogleMobileAdsConsentManager googleMobileAdsConsentManager =
         GoogleMobileAdsConsentManager.getInstance(getApplicationContext());
