fcff77601e28943fce220d06c068308d9a686a81
Nicholas Ventimiglia <nventimiglia@google.com>
2024-08-27 10:50:10 -0700

    Added jetpack compose 'compose-support' module.
    PiperOrigin-RevId: 668052867

--- kotlin/advanced/JetpackComposeDemo/build.gradle.kts
+++ kotlin/advanced/JetpackComposeDemo/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
   id("com.android.application") version "8.2.2" apply false
   id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+  id("com.android.library") version "8.2.2" apply false
 }
 
 tasks.register("clean", Delete::class) { delete(rootProject.buildDir) }
