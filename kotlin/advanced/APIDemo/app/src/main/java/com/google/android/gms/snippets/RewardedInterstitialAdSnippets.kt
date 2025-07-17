e3124800aae80b956b23ebae0497d51abd676160
Justin Malandruccolo <jmalandruccolo@google.com>
2025-07-17 08:11:00 -0700

    Added region tags to Android Rewarded Interstitial snippets
    PiperOrigin-RevId: 784185292

---
+++ kotlin/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/RewardedInterstitialAdSnippets.kt
@@ -0,0 +1,50 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.android.gms.snippets
+
+import android.content.Context
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.rewarded.ServerSideVerificationOptions
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
+
+/** Kotlin code snippets for the developer guide. */
+private class RewardedInterstitialAdSnippets {
+
+  private var rewardedInterstitialAd: RewardedInterstitialAd? = null
+
+  private fun validateServerSideVerfification(context: Context) {
+    // [START validate_server_side_verification]
+    RewardedInterstitialAd.load(
+      context,
+      AD_UNIT_ID,
+      AdRequest.Builder().build(),
+      object : RewardedInterstitialAdLoadCallback() {
+        override fun onAdLoaded(ad: RewardedInterstitialAd) {
+          rewardedInterstitialAd = ad
+          val options =
+            ServerSideVerificationOptions.Builder().setCustomData(SAMPLE_CUSTOM_DATA_STRING).build()
+          rewardedInterstitialAd?.setServerSideVerificationOptions(options)
+        }
+      },
+    )
+    // [END validate_server_side_verification]
+  }
+
+  private companion object {
+    const val AD_UNIT_ID = "ca-app-pub-3940256099942544/5354046379"
+    const val SAMPLE_CUSTOM_DATA_STRING = "SAMPLE_CUSTOM_DATA_STRING"
+  }
+}
