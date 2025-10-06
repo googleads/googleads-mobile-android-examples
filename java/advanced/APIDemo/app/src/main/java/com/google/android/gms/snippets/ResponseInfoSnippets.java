1d6cc605c4f37468d900bc8de1b8a25fcdd88656
Justin Malandruccolo <jmalandruccolo@google.com>
2025-10-06 09:32:36 -0700

    Added more responseInfo snippets
    PiperOrigin-RevId: 815761618

--- java/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/ResponseInfoSnippets.java
+++ java/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/ResponseInfoSnippets.java
@@ -14,16 +14,84 @@
 
 package com.google.android.gms.snippets;
 
+import android.os.Bundle;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import com.google.android.gms.ads.AdError;
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.AdapterResponseInfo;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.ResponseInfo;
+import java.util.List;
 
 /** Java code snippets for the developer guide. */
 public class ResponseInfoSnippets {
 
   private static final String TAG = "ResponseInfoSnippets";
+
+  private void logResponseInfo(AdView adView) {
+    adView.setAdListener(
+        new AdListener() {
+          // [START log_response_info]
+          @Override
+          public void onAdLoaded() {
+            ResponseInfo responseInfo = adView.getResponseInfo();
+            Log.d(TAG, responseInfo.toString());
+          }
+
+          @Override
+          public void onAdFailedToLoad(LoadAdError adError) {
+            ResponseInfo responseInfo = adError.getResponseInfo();
+            Log.d(TAG, responseInfo.toString());
+          }
+          // [END log_response_info]
+        });
+  }
+
+  private void examineResponseInfo(AdView adView) {
+    adView.setAdListener(
+        new AdListener() {
+          // [START get_response_info]
+          @Override
+          public void onAdLoaded() {
+            ResponseInfo responseInfo = adView.getResponseInfo();
+
+            String responseId = responseInfo.getResponseId();
+            String mediationAdapterClassName = responseInfo.getMediationAdapterClassName();
+            List<AdapterResponseInfo> adapterResponses = responseInfo.getAdapterResponses();
+            AdapterResponseInfo loadedAdapterResponseInfo =
+                responseInfo.getLoadedAdapterResponseInfo();
+            Bundle extras = responseInfo.getResponseExtras();
+            String mediationGroupName = extras.getString("mediation_group_name");
+            String mediationABTestName = extras.getString("mediation_ab_test_name");
+            String mediationABTestVariant = extras.getString("mediation_ab_test_variant");
+          }
+          // [END get_response_info]
+        });
+  }
+
+  private void examineAdapterResponseInfo(AdView adView) {
+    adView.setAdListener(
+        new AdListener() {
+          // [START get_adapter_response_info]
+          @Override
+          public void onAdLoaded() {
+            AdapterResponseInfo loadedAdapterResponseInfo =
+                adView.getResponseInfo().getLoadedAdapterResponseInfo();
+
+            AdError adError = loadedAdapterResponseInfo.getAdError();
+            String adSourceId = loadedAdapterResponseInfo.getAdSourceId();
+            String adSourceInstanceId = loadedAdapterResponseInfo.getAdSourceInstanceId();
+            String adSourceInstanceName = loadedAdapterResponseInfo.getAdSourceInstanceName();
+            String adSourceName = loadedAdapterResponseInfo.getAdSourceName();
+            String adapterClassName = loadedAdapterResponseInfo.getAdapterClassName();
+            Bundle credentials = loadedAdapterResponseInfo.getCredentials();
+            long latencyMillis = loadedAdapterResponseInfo.getLatencyMillis();
+          }
+          // [END get_adapter_response_info]
+        });
+  }
 
   // [START get_ad_source_name]
   private String getUniqueAdSourceName(@NonNull AdapterResponseInfo loadedAdapterResponseInfo) {
@@ -41,7 +109,7 @@ public class ResponseInfoSnippets {
 
   // [END get_ad_source_name]
 
-  private void getMediationAdapterClassNameFromAd(AdView ad) {
+  private void logMediationAdapterClassNameFromAd(AdView ad) {
     // [START get_adapter_class_name]
     ResponseInfo responseInfo = ad.getResponseInfo();
     String adapterClassName = null;
--- kotlin/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/ResponseInfoSnippets.kt
+++ kotlin/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/ResponseInfoSnippets.kt
@@ -15,11 +15,71 @@
 package com.google.android.gms.snippets
 
 import android.util.Log
+import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.AdapterResponseInfo
+import com.google.android.gms.ads.LoadAdError
 
 /** Kotlin code snippets for the developer guide. */
 class ResponseInfoSnippets {
+
+  private fun logResponseInfo(adView: AdView) {
+    adView.adListener =
+      object : AdListener() {
+        // [START log_response_info]
+        override fun onAdLoaded() {
+          val responseInfo = adView.responseInfo
+          Log.d(TAG, responseInfo.toString())
+        }
+
+        override fun onAdFailedToLoad(adError: LoadAdError) {
+          val responseInfo = adError.responseInfo
+          Log.d(TAG, responseInfo.toString())
+        }
+        // [END log_response_info]
+      }
+  }
+
+  private fun useResponseInfo(adView: AdView) {
+    adView.adListener =
+      object : AdListener() {
+        // [START get_response_info]
+        override fun onAdLoaded() {
+          val responseInfo = adView.responseInfo
+
+          val responseId = responseInfo?.responseId
+          val mediationAdapterClassName = responseInfo?.mediationAdapterClassName
+          val adapterResponses = responseInfo?.adapterResponses
+          val loadedAdapterResponseInfo = responseInfo?.loadedAdapterResponseInfo
+          val extras = responseInfo?.responseExtras
+          val mediationGroupName = extras?.getString("mediation_group_name")
+          val mediationABTestName = extras?.getString("mediation_ab_test_name")
+          val mediationABTestVariant = extras?.getString("mediation_ab_test_variant")
+        }
+        // [END get_response_info]
+      }
+  }
+
+  private fun useAdapterResponseInfo(adView: AdView) {
+    adView.adListener =
+      object : AdListener() {
+        // [START get_adapter_response_info]
+        override fun onAdLoaded() {
+          val loadedAdapterResponseInfo = adView.responseInfo?.loadedAdapterResponseInfo
+
+          val adError = loadedAdapterResponseInfo?.adError
+          val adSourceId = loadedAdapterResponseInfo?.adSourceId
+          val adSourceInstanceId = loadedAdapterResponseInfo?.adSourceInstanceId
+          val adSourceInstanceName = loadedAdapterResponseInfo?.adSourceInstanceName
+          val adSourceName = loadedAdapterResponseInfo?.adSourceName
+          val adapterClassName = loadedAdapterResponseInfo?.adapterClassName
+          val credentials = loadedAdapterResponseInfo?.credentials
+          val latencyMillis = loadedAdapterResponseInfo?.latencyMillis
+        }
+        // [END get_adapter_response_info]
+      }
+  }
+
   // [START get_ad_source_name]
   private fun getUniqueAdSourceName(loadedAdapterResponseInfo: AdapterResponseInfo): String {
 
