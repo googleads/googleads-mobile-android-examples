62c41d92d765cdddaf55a4e38bdb8924b87a1570
Justin Malandruccolo <jmalandruccolo@google.com>
2025-10-10 06:46:00 -0700

    Update Java & Kotlin samples to Google Mobile Ads SDK version 24.7.0
    PiperOrigin-RevId: 817623355

--- kotlin/advanced/JetpackComposeDemo/compose-util/build.gradle.kts
+++ kotlin/advanced/JetpackComposeDemo/compose-util/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
   implementation("androidx.compose.ui:ui-graphics:1.8.0")
   implementation("androidx.compose.material3:material3")
   implementation("androidx.compose.foundation:foundation")
-  implementation("com.google.android.gms:play-services-ads:24.6.0")
+  implementation("com.google.android.gms:play-services-ads:24.7.0")
   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
   debugImplementation("androidx.compose.ui:ui-tooling")
 }
