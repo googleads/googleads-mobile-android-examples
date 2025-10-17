0792c677cd3e4704991fa3d0a2fa85f4451d27b8
Jason Chiu <jasoncsh@google.com>
2025-10-16 17:55:51 -0700

    Internal change.
    PiperOrigin-RevId: 820461951

---
+++ java/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/prerelease/TestSnippet.java
@@ -0,0 +1,8 @@
+package com.google.android.gms.snippets.prerelease;
+
+/** Simple class for testing Copybara exclusions. */
+public class TestSnippet {
+  public String getGreeting() {
+    return "Hello World";
+  }
+}
---
+++ kotlin/advanced/APIDemo/app/src/main/java/com/google/android/gms/snippets/prerelease/TestSnippet.kt
@@ -0,0 +1,8 @@
+package com.google.android.gms.snippets.prerelease
+
+/** Simple class for testing Copybara exclusions. */
+class TestSnippet {
+  fun getGreeting(): String {
+    return "Hello World"
+  }
+}
