210e4cff9ebe967498985973cc2b2258e1f112cc
Nicholas Ventimiglia <nventimiglia@google.com>
2026-01-13 11:26:42 -0800

    Updated gradle manifests for Inline Adaptive Banner sample.
    PiperOrigin-RevId: 855810928

--- kotlin/advanced/InlineAdaptiveBannerExample/app/build.gradle.kts
+++ kotlin/advanced/InlineAdaptiveBannerExample/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
   namespace = "com.google.android.gms.example.inlineadaptivebannerexample"
-  compileSdk = 34
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.google.android.gms.example.inlineadaptivebannerexample"
     minSdk = 23
-    targetSdk = 34
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
   }
@@ -27,16 +27,16 @@ android {
     targetCompatibility = JavaVersion.VERSION_1_8
   }
 
-  kotlinOptions { jvmTarget = "1.8" }
-
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
+kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8) } }
+
 dependencies {
-  implementation("androidx.core:core-ktx:1.12.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("androidx.core:core-ktx:1.16.0")
+  implementation("androidx.appcompat:appcompat:1.7.1")
   implementation("androidx.cardview:cardview:1.0.0")
-  implementation("androidx.recyclerview:recyclerview:1.3.2")
+  implementation("androidx.recyclerview:recyclerview:1.4.0")
   implementation("com.google.android.gms:play-services-ads:24.9.0")
   implementation("com.google.android.material:material:1.13.0")
 }
--- kotlin/advanced/InlineAdaptiveBannerExample/build.gradle.kts
+++ kotlin/advanced/InlineAdaptiveBannerExample/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
   id("com.android.application") version "8.6.0" apply false
   id("com.android.library") version "8.6.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+  id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 tasks.register("clean", Delete::class) { delete(rootProject.buildDir) }
