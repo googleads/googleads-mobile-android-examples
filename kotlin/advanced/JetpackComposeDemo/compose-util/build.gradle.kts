1b640d2d465a86958f634cf3b4c5d09c19d9c115
Jason Chiu <jasoncsh@google.com>
2026-06-22 14:57:06 -0700

    Updates Android samples to use v25.4.0
    PiperOrigin-RevId: 936265461

--- kotlin/advanced/JetpackComposeDemo/compose-util/build.gradle.kts
+++ kotlin/advanced/JetpackComposeDemo/compose-util/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
   implementation("androidx.compose.foundation:foundation")
-  implementation("com.google.android.gms:play-services-ads:25.3.0")
+  implementation("com.google.android.gms:play-services-ads:25.4.0")
   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
   debugImplementation("androidx.compose.ui:ui-tooling")
 }
